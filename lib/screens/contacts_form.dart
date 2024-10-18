a94604eff908706ce5e3f657d9af2fbb74e5d5b6
XandyGomes <x3.computadores@hotmail.com>
2024-10-17 21:14:23 -0300

    Aula. 17.10.2024

---
+++ lib/screens/contacts_form.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ContactForm extends StatelessWidget {
+  @override
+  Widget build(Object context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Novo Contato'),
+      ),
+    );
+  }
+}
